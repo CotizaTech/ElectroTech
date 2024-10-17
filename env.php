6eb3361b750b396a64a5ed16e9682ff856a148db
Fredy Quej <fquejq@miumg.edu.gt>
2024-10-17 00:08:18 -0600

    :alien: Se agrego env.php conexion a la base de datos

---
+++ env.php
@@ -0,0 +1,22 @@
+; <?php exit; ?>
+
+; URL base del proyecto, algo como https://sitio.com
+BASE_URL = "http://localhost/cotizaciones"
+
+
+USUARIO_MYSQL = "root"
+PASS_MYSQL = "fquejq"
+NOMBRE_BD_MYSQL = "cotizaciones"
+HOST_MYSQL = "localhost"
+
+
+USUARIO_MYSQL_SESION = "root"
+PASS_MYSQL_SESION = "fquejq"
+NOMBRE_BD_MYSQL_SESION = "cotizaciones"
+HOST_MYSQL_SESION = "localhost"
+
+; De Google Analytics
+ID_SEGUIMIENTO = ""
+; Publicidad
+AD_CLIENT = ""
+AD_SLOT = ""
