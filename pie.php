44b6ef0c1c49ce9cbf3d400cf15660f1d2f10d75
Fredy Quej <fquejq@miumg.edu.gt>
2024-10-19 22:56:25 -0600

    :sparkles: Una nueva caracteristica y funcionalidad

---
+++ pie.php
@@ -0,0 +1,15 @@
+</div>
+<footer class="footer text-center d-print-none footer-banner">
+    <div class="container">
+        <span class="text-muted">Generador de cotizaciones <strong><a
+                        target="_blank" href=" ">ANALISIS DE SISTEMA II-UMG COBAN</a></strong></strong>.
+            <br>
+            Visita el <a target="_blank" href="https://github.com/CotizaTech/ElectroTech.git">Repositorio en GitHub <i
+                        class="fa fa-github"></i></a>
+        </span>
+    </div>
+</footer>
+
+</body>
+
+</html>
