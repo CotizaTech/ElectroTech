670ccf32551e0d4814c763a2cd567413923f8e85
Fredy Quej <fquejq@miumg.edu.gt>
2024-10-20 16:09:16 -0600

    :alien: Modulos adicionales para mejorar la funcionalidad

---
+++ includes/creditos.php
@@ -0,0 +1,25 @@
+<div class="row d-flex justify-content-center align-items-center">
+    <div class="col-sm text-center">
+        <h1>Sistema web para cotizaciones</h1>
+        <p></p>
+        <h2></h2>
+        <p><a target="_blank" href=></a>.
+            
+            <a target="_blank" href="https://github.com/CotizaTech/ElectroTech.git">repositorio de GitHub</a>.
+            <a target="_blank" href=></a>.
+        </p>
+        <h2></h2>
+        <p></p>
+        <ul>
+            <li><a href="" target="_blank"></a></li>
+            <li><a href="" target="_blank"></a></li>
+            <li><a href="" target="_blank"></a></li>
+            <li><a href="" target="_blank"></a></li>
+        </ul>
+        <h2>Contacto</h2>
+        <p>
+            
+             <a target="_blank" href=""></a>.
+        </p>
+    </div>
+</div>
