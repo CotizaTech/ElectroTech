48e45fe40758f30c5c0bc813b5557e1ee1e5eab3
Fredy Quej <fquejq@miumg.edu.gt>
2024-10-19 23:05:34 -0600

    :children_crossing: Una nueva caracteristica Registro de Usuario

---
+++ includes/registro.php
@@ -0,0 +1,72 @@
+<?php
+$tokenCSRF = Utiles::obtenerTokenCSRF();
+?>
+<head>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+
+<div class="row" style="margin-top: 1%;">
+    <div class="col-sm-12 col-lg-4 offset-lg-4">
+        <h2 style="color: #007BFF;">Registro</h2>
+        <div class="alert alert-info">
+            Regístrate a Sistema de Cotizaciones 
+        </div>
+    </div>
+</div>
+<div class="row" id="app">
+    <div class="col-sm-12 col-lg-4 offset-lg-4">
+        <form ref="form" method="post" action="<?php echo BASE_URL ?>/?p=guardar_usuario">
+            <input type="hidden" name="tokenCSRF" value="<?php echo $tokenCSRF ?>">
+            <div class="form-group">
+                <label for="correo">Gmail</label>
+                <input autofocus name="correo" autocomplete="off" required type="email" class="form-control"
+                       id="correo" placeholder="Ingrese su correo">
+            </div>
+            <div class="form-group">
+                <label for="pass">Contraseña</label>
+                <input v-model="usuario.pass" name="pass" autocomplete="off" required type="password"
+                       class="form-control"
+                       id="pass" placeholder="Escribe su contraseña">
+            </div>
+            <div class="form-group">
+                <label for="pass2">Confirma tu contraseña</label>
+                <input v-model="usuario.pass2" name="pass2" autocomplete="off" required type="password"
+                       class="form-control"
+                       id="pass2" placeholder="Vuelve a escribir su contraseña">
+            </div>
+            <button type="button" @click="registrar" class="btn btn-primary">Registrarme</button>
+            <br>
+            <a href="<?php echo BASE_URL ?>?p=login">Ya tengo una cuenta</a>
+        </form>
+    </div>
+    <div class="col-sm-12 col-lg-4 offset-lg-4" v-show="mostrarAlertaPassNoCoincide">
+        <br>
+        <div class="alert alert-danger">
+            Las contraseñas no coinciden
+        </div>
+    </div>
+</div>
+<script>
+    new Vue({
+        el: "#app",
+        data: () => ({
+            usuario: {
+                pass: "",
+                pass2: "",
+            },
+            mostrarAlertaPassNoCoincide: false,
+        }),
+        methods: {
+            registrar() {
+                this.mostrarAlertaPassNoCoincide = false;
+                if (!this.$refs.form.checkValidity()) {
+                    return this.$refs.form.reportValidity();
+                }
+                if (this.usuario.pass && (this.usuario.pass !== this.usuario.pass2)) {
+                    return this.mostrarAlertaPassNoCoincide = true;
+                }
+                this.$refs.form.submit();
+            }
+        }
+    });
+</script>
